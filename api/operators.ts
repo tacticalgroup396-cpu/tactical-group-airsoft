7add87115ba463991ae7a14d6c1cf490a57eddf4
tacticalgroup396-cpu <tacticalgroup396@gmail.com>
2026-08-19 23:20:56 -0300

    Fix API module resolution in operators.ts

--- api/operators.ts
+++ api/operators.ts
@@ -1,6 +1,6 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 import bcrypt from "bcryptjs";
-import { sql } from "./_db";
+import { sql } from "./_db.ts";
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   try {
