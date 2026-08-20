6ec701f1ec4eb5fb3e323c140f7c743334839e5e
tacticalgroup396-cpu <tacticalgroup396@gmail.com>
2026-08-19 23:21:14 -0300

    Fix API module resolution in rules.ts

--- api/rules.ts
+++ api/rules.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
-import { sql } from "./_db";
+import { sql } from "./_db.ts";
 
 export default async function handler(_req: VercelRequest, res: VercelResponse) {
   try {
