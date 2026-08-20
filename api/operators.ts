b70b22f159334475bc52d15fce7fe987fbce7cfd
tacticalgroup396-cpu <tacticalgroup396@gmail.com>
2026-08-19 23:29:42 -0300

    Update operators.ts

--- api/operators.ts
+++ api/operators.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
-import bcrypt from "bcryptjs";
+from "./db.js"
 import { sql } from "./_db.ts";
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
