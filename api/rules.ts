6d6240cb9b4ae3ca5879093c302e3873a9ed0405
tacticalgroup396-cpu <tacticalgroup396@gmail.com>
2026-08-19 23:29:07 -0300

    Update rules.ts

--- api/rules.ts
+++ api/rules.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
-import { sql } from "./_db.ts";
+from "./db.js"
 
 export default async function handler(_req: VercelRequest, res: VercelResponse) {
   try {
